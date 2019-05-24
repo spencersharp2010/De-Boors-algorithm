dc60136af2982bab2e0078c42bb6cd356f485acb
Philipp Kopp <philipp.kopp@tum.de>
2019-05-24 14:48:05 +0200

    Minor change

--- python/bindings/splinekernel_python.cpp
+++ python/bindings/splinekernel_python.cpp
@@ -11,5 +11,4 @@ PYBIND11_MODULE( pysplinekernel, m )
 
     m.def( "evaluateBSplineBasis", &cie::splinekernel::evaluateBSplineBasis, "Evaluates single b-spline basis function." );
     m.def( "evaluate2DCurve", &cie::splinekernel::evaluate2DCurve, "Evaluates B-Spline curve by multiplying control points and basis functions." );
-    m.def( "evaluate2DCurveDeBoor", &cie::splinekernel::evaluate2DCurveDeBoor, "Evaluates B-Spline using DeBoor" );
 }
