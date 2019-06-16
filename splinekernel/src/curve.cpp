4c73015e0748c26de3ca3dbc378690d459a26392
Spencer Sharp <ge73ver@mytum.de>
2019-06-16 21:36:24 +0200

    working system tests

--- splinekernel/src/curve.cpp
+++ splinekernel/src/curve.cpp
@@ -52,14 +52,42 @@ std::array<double, 2> deBoor( double t,
                               const std::vector<double>& yCoordinates,
                               size_t recursionLevel )
 {
-    return { 0.0, 0.0 };
+    if(recursionLevel == polynomialDegree + 1)
+    {
+        return {xCoordinates[knotSpanIndex], yCoordinates[knotSpanIndex]};
+    }
+
+    double gamma = (t - knotVector[knotSpanIndex])/(knotVector[knotSpanIndex+recursionLevel] - knotVector[knotSpanIndex]);
+    std::array<double, 2> dummy1 = deBoor(t, knotSpanIndex - 1, polynomialDegree, knotVector, xCoordinates, yCoordinates, recursionLevel + 1);
+    std::array<double, 2> dummy2 = deBoor(t, knotSpanIndex, polynomialDegree, knotVector, xCoordinates, yCoordinates, recursionLevel + 1);
+    double resultX = (1 - gamma) * dummy1[0] + gamma * dummy2[0];
+    double resultY = (1 - gamma) * dummy1[1] + gamma * dummy2[1];
+
+    return { resultX, resultY };
 }
 
 size_t findKnotSpan( double t,
                      size_t numberOfControlPoints,
                      const std::vector<double>& knotVector )
 {
-    return 0.0;
+    size_t index = 0;
+    int size_of_knot_vector = knotVector.size();
+
+    if (t > knotVector[size_of_knot_vector-1])
+    {
+        throw std::runtime_error("invalid input");
+    }
+    while(t >= knotVector[index])
+    {
+        if(index >= numberOfControlPoints)
+        {
+            break;
+        }
+        index++;
+    }
+
+
+    return index-1;
 }
 
 std::array<std::vector<double>, 2> evaluate2DCurveDeBoor( const std::vector<double>& tCoordinates,
@@ -67,7 +95,33 @@ std::array<std::vector<double>, 2> evaluate2DCurveDeBoor( const std::vector<doub
                                                           const std::vector<double>& yCoordinates,
                                                           const std::vector<double>& knotVector )
 {
-    return { std::vector<double>{ }, std::vector<double>{ } };
+    size_t numberOfSamples = tCoordinates.size( );
+    size_t numberOfPoints = xCoordinates.size( );
+    size_t m = knotVector.size( );
+    size_t p = m - numberOfPoints - 1;
+    std::array<std::vector<double>, 2> returnValues;
+
+    returnValues[0].reserve(numberOfSamples);
+    returnValues[1].reserve(numberOfSamples);
+
+    if( yCoordinates.size( ) != numberOfPoints )
+    {
+        throw std::runtime_error( "Inconsistent size in evaluate2DCurve." );
+    }
+
+    for(size_t i=0; i < numberOfSamples; i++)
+    {
+        size_t knotSpanIndex_x = findKnotSpan(tCoordinates[i],numberOfPoints,knotVector);
+
+        std::array<double, 2> dummy = deBoor(tCoordinates[i],knotSpanIndex_x,p,knotVector,xCoordinates,yCoordinates);
+        returnValues[0].push_back(dummy[0]);
+        returnValues[1].push_back(dummy[1]);
+
+
+    }
+
+    return {returnValues};
+    //return { std::vector<double>{ }, std::vector<double>{ } };
 }
 
 } // namespace splinekernel
